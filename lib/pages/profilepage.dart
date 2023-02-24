c86eb7fb7f4dbb9035cb48a035066f061982c620
이준선 <nx006@ijunseon-ui-MacBookPro.local>
2023-02-24 18:34:18 +0900

    Add profile account view

--- lib/pages/profilepage.dart
+++ lib/pages/profilepage.dart
@@ -8,12 +8,64 @@ class ProfilePage extends StatefulWidget {
 }
 
 class _ProfilePageState extends State<ProfilePage> {
+  static const double _profileHeight = 250;
+  static const double _profileRound = 40;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-        child: Text(
-      'Profile Page',
-      style: TextStyle(fontSize: 24),
-    ));
+    const textProfileUserStyle =
+        TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+    return ListView(
+      children: <Widget>[
+        Container(
+            height: _profileHeight,
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 10,
+                  spreadRadius: 10,
+                ),
+              ],
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(_profileRound),
+                topRight: Radius.circular(_profileRound),
+                bottomLeft: Radius.circular(_profileRound),
+                bottomRight: Radius.circular(_profileRound),
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    const CircleAvatar(
+                      minRadius: 60.0,
+                      child: CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage:
+                            AssetImage('assets/images/profileImage.jpeg'),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: const [
+                          Text('User Name', style: textProfileUserStyle),
+                          Text('Explain', style: TextStyle(fontSize: 20)),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ))
+      ],
+    );
   }
 }
