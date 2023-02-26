28cf9d7a79202e4b52fe45ea4fa2dda2f77923ae
이준선 <nx006@ijunseon-ui-MacBookPro.local>
2023-02-26 17:43:59 +0900

    Add communities a location field

--- lib/models/communities.dart
+++ lib/models/communities.dart
@@ -1,9 +1,15 @@
-
 /// Community model
 /// This model is used to store the community information
 class Community {
   final int communityId;
   final String communityName;
 
-  Community({required this.communityId, required this.communityName});
+  /// location of the community
+  String? communityLocation;
+
+  Community({
+    required this.communityId,
+    required this.communityName,
+    this.communityLocation,
+  });
 }
