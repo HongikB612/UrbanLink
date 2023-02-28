18aa4b220d3da13f3137f3853f38f0b806526cf1
이준선 <nx006@ijunseon-ui-MacBookPro.local>
2023-02-28 19:19:15 +0900

    Feat add search bar

--- lib/pages/mappage.dart
+++ lib/pages/mappage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:anim_search_bar/anim_search_bar.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -9,6 +10,7 @@ class MapPage extends StatefulWidget {
 }
 
 class _MapPageState extends State<MapPage> {
+  TextEditingController textController = TextEditingController();
   late GoogleMapController mapController;
 
   final LatLng _center = const LatLng(45.521563, -122.677433);
@@ -19,20 +21,50 @@ class _MapPageState extends State<MapPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('장소'),
-          backgroundColor: Colors.blue,
-        ),
-        body: GoogleMap(
+    return Stack(
+      children: <Widget>[
+        GoogleMap(
           onMapCreated: _onMapCreated,
           initialCameraPosition: CameraPosition(
             target: _center,
             zoom: 11.0,
           ),
         ),
-      ),
+        Positioned(
+          top: 10,
+          right: 15,
+          left: 15,
+          child: Container(
+            color: Colors.white,
+            child: Row(
+              children: <Widget>[
+                IconButton(
+                  splashColor: Colors.grey,
+                  icon: const Icon(Icons.menu),
+                  onPressed: () {},
+                ),
+                const Expanded(
+                  child: TextField(
+                    cursorColor: Colors.black,
+                    keyboardType: TextInputType.text,
+                    textInputAction: TextInputAction.go,
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        contentPadding: EdgeInsets.symmetric(horizontal: 15),
+                        hintText: "Search..."),
+                  ),
+                ),
+                IconButton(
+                  icon: const Icon(Icons.search),
+                  onPressed: () {
+                    setState(() {});
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
