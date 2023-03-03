c7897090510ecde61d901ae8d7691393b66403a6
이준선 <nx006@ijunseon-ui-MacBookPro.local>
2023-03-03 23:55:05 +0900

    Feat add searching based on address query given. But not yet implemented how to solve getting error when unspecified query is given. Work well only the valid query for now on.

--- lib/pages/mappage/mappage.dart
+++ lib/pages/mappage/mappage.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:logger/logger.dart';
+import 'package:urbanlink_project/services/auth.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -13,43 +17,67 @@ class _MapPageState extends State<MapPage> {
   TextEditingController textController = TextEditingController();
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  LatLng _center = const LatLng(45.521563, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    setState(() {
+      mapController = controller;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Map'),
-        ),
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
+      appBar: AppBar(
+        title: const Text('Map'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
             ),
-            Positioned(
-              top: 5,
-              right: 15,
-              left: 15,
-              child: AnimSearchBar(
-                width: MediaQuery.of(context).size.width,
-                textController: textController,
-                onSuffixTap: () {
-                  setState(() {
-                    textController.clear();
-                  });
-                },
-                onSubmitted: (String _) {},
-              ),
+          ),
+          Positioned(
+            top: 5,
+            right: 15,
+            left: 15,
+            child: AnimSearchBar(
+              width: MediaQuery.of(context).size.width,
+              textController: textController,
+              onSuffixTap: () {
+                setState(() {
+                  textController.clear();
+                });
+              },
+              onSubmitted: (String value) {
+                _searchLocation(value);
+              },
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _searchLocation(String value) async {
+    late List<Location> locations;
+    try {
+      locations = await locationFromAddress(value);
+    } on PlatformException catch (e) {
+      logger.e(e);
+    }
+
+    if (locations.isNotEmpty) {
+      final LatLng newCenter =
+          LatLng(locations.first.latitude, locations.first.longitude);
+
+      mapController.animateCamera(CameraUpdate.newLatLng(newCenter));
+      setState(() {
+        _center = newCenter;
+      });
+    }
   }
 }
