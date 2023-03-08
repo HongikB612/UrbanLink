4027b1dd6f8dc5598c6672b7324c1f915a84526f
이준선 <nx006@ijunseon-ui-MacBookPro.local>
2023-03-09 06:57:37 +0900

    Remove unused method

--- lib/pages/mappage/mappage.dart
+++ lib/pages/mappage/mappage.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:urbanlink_project/services/auth.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -13,43 +16,65 @@ class _MapPageState extends State<MapPage> {
   TextEditingController textController = TextEditingController();
   late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  LatLng _center = const LatLng(37.7, 126.6);
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    setState(() {
+      mapController = controller;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Map'),
-        ),
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
+      appBar: AppBar(
+        title: const Text('Map'),
+      ),
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
             ),
-            Positioned(
-              top: 5,
-              right: 15,
-              left: 15,
-              child: AnimSearchBar(
-                width: MediaQuery.of(context).size.width,
-                textController: textController,
-                onSuffixTap: () {
-                  setState(() {
-                    textController.clear();
-                  });
-                },
-                onSubmitted: (String _) {},
-              ),
+          ),
+          Positioned(
+            top: 5,
+            right: MediaQuery.of(context).size.width * 0.05,
+            left: MediaQuery.of(context).size.width * 0.05,
+            child: AnimSearchBar(
+              width: MediaQuery.of(context).size.width,
+              textController: textController,
+              onSuffixTap: () {
+                setState(() {
+                  textController.clear();
+                });
+              },
+              onSubmitted: (String value) {
+                _searchLocation(value);
+              },
             ),
-          ],
-        ));
+          ),
+        ],
+      ),
+    );
+  }
+
+  Future<void> _searchLocation(String query) async {
+    try {
+      List<Location> locations = await locationFromAddress(query);
+
+      if (locations.isNotEmpty) {
+        final LatLng newCenter =
+            LatLng(locations.first.latitude, locations.first.longitude);
+
+        setState(() {
+          _center = newCenter;
+        });
+      }
+    } catch (e) {
+      logger.e(e);
+    }
   }
 }
