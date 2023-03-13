d92febd793a898e3d60d89508b32c7bce21f6e6c
nx006 <limazero14@gmail.com>
2023-03-14 00:58:41 +0900

    Feat Posting the post add user id to database

--- lib/pages/postpage/postingpage.dart
+++ lib/pages/postpage/postingpage.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:urbanlink_project/repositories/post_database_service.dart';
@@ -9,6 +10,14 @@ class PostingPage extends StatelessWidget {
   Widget build(BuildContext context) {
     TextEditingController headlineController = TextEditingController();
     TextEditingController contentController = TextEditingController();
+
+    // user
+    late User user;
+    if (FirebaseAuth.instance.currentUser != null) {
+      user = FirebaseAuth.instance.currentUser!;
+    } else {
+      Get.back(result: '로그인이 필요합니다.');
+    }
 
     return Scaffold(
       appBar: AppBar(
@@ -39,14 +48,14 @@ class PostingPage extends StatelessWidget {
               onPressed: () {
                 PostDatabaseService.createPost(
                   communityId: '',
-                  postAuthorId: '',
+                  postAuthorId: user.uid,
                   postContent: contentController.text,
                   locationId: '',
                   postCreatedTime: DateTime.now(),
                   postLastModified: DateTime.now(),
                   postTitle: headlineController.text,
                 );
-                Get.back(result: headlineController.text);
+                Get.back(result: '게시글이 등록되었습니다.');
               },
               child: const Text('게시하기'),
             ),
