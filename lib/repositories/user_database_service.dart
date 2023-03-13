d5d9bdcb4e7aa9761169dd6ecee4fa35e9388678
nx006 <limazero14@gmail.com>
2023-03-13 13:50:31 +0900

    Feat create User database service

--- lib/repositories/user_database_service.dart
+++ lib/repositories/user_database_service.dart
@@ -1,1 +1,20 @@
-class UserDatabaseService {}
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class UserDatabaseService {
+  static Future<void> createUser(
+      {required String uid,
+      required String name,
+      required String email}) async {
+    final docUser = FirebaseFirestore.instance.collection('users').doc(uid);
+
+    final json = {
+      'userId': docUser.id,
+      'userName': name,
+      'userEmail': email,
+      'userExplanation': '',
+    };
+
+    // create document and write data to Firebase
+    await docUser.set(json);
+  }
+}
