9dad9a073dbc92a6c4ee9a865ee60c2a3c4cd239
SoHyun Lee <2887thgus@gmail.com>
2023-03-18 00:10:38 +0900

    feat(mappage) : add asynchronous features and a button named to the home

--- lib/pages/mappage/mappage.dart
+++ lib/pages/mappage/mappage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
+import 'dart:async';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -11,13 +12,21 @@ class MapPage extends StatefulWidget {
 
 class _MapPageState extends State<MapPage> {
   TextEditingController textController = TextEditingController();
-  late GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-  }
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  static const CameraPosition _kLake = CameraPosition(
+      bearing: 192.8334901395799,
+      target: LatLng(37.43296265331129, -122.08832357078792),
+      tilt: 59.440717697143555,
+      zoom: 19.151926040649414
+  );
+  //
 
   @override
   Widget build(BuildContext context) {
@@ -25,15 +34,18 @@ class _MapPageState extends State<MapPage> {
         appBar: AppBar(
           title: const Text('Map'),
         ),
+
         body: Stack(
           children: <Widget>[
             GoogleMap(
-              onMapCreated: _onMapCreated,
-              initialCameraPosition: CameraPosition(
-                target: _center,
-                zoom: 11.0,
-              ),
+              mapType: MapType.terrain,
+              onMapCreated: (GoogleMapController controller) {
+                _controller.complete(controller);
+              },
+              initialCameraPosition: _kGooglePlex,
+            //markers: Set<Marker>.of(markers.values)),
             ),
+
             Positioned(
               top: 5,
               right: 15,
@@ -50,6 +62,16 @@ class _MapPageState extends State<MapPage> {
               ),
             ),
           ],
-        ));
+        ),
+        floatingActionButton: FloatingActionButton.extended(
+        onPressed: _goToTheHome,
+        label: const Text('To the home!'),
+        icon: const Icon(Icons.directions_boat),
+      ),
+    );
+  }
+  Future<void> _goToTheHome() async {
+    final GoogleMapController controller = await _controller.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(_kLake));
   }
 }
