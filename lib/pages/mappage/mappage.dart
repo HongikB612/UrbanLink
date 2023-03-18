648abb93ef4b3ff60fee8be2fbfabdb4073bbf0c
SoHyun Lee <2887thgus@gmail.com>
2023-03-18 15:43:31 +0900

    refactor(mappage) : use get to route

--- lib/pages/mappage/mappage.dart
+++ lib/pages/mappage/mappage.dart
@@ -2,7 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
+import 'package:urbanlink_project/pages/postpage/postspage.dart';
 import 'dart:async';
+import 'package:get/get.dart';
+
+import 'package:urbanlink_project/pages/postpage/postspage.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({Key? key}) : super(key: key);
@@ -38,7 +42,7 @@ class _MapPageState extends State<MapPage> {
       markerId: MarkerId("1"),
       onTap: () => {
         print("1"),
-        Navigator.of(context).pushNamed('/posts')
+        Get.to(const PostsPage()),
       },
       position: LatLng(	37.552635722509, 126.92436042413),
       icon: customMarkerIcon,
@@ -48,7 +52,7 @@ class _MapPageState extends State<MapPage> {
       markerId: MarkerId("2"),
       onTap: () => {
         print("2"),
-        Navigator.of(context).pushNamed('/posts')
+        Get.to(const PostsPage()),
       },
       position: LatLng(	37.565643683342, 126.95524147826),
       icon: customMarkerIcon,
@@ -58,7 +62,7 @@ class _MapPageState extends State<MapPage> {
       markerId: MarkerId("3"),
       onTap: () => {
         print("3"),
-        Navigator.of(context).pushNamed('/posts')
+        Get.to(const PostsPage()),
       },
       position: LatLng(	37.495172947072, 126.95453489844),
       icon: customMarkerIcon,
