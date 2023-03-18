a6212a7b900de69abf496684caf7e400323a037e
nx006 <limazero14@gmail.com>
2023-03-18 21:02:18 +0900

    Refactor Remove unused field

--- lib/pages/profilepage/profilepage.dart
+++ lib/pages/profilepage/profilepage.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:urbanlink_project/repositories/user_database_service.dart';
