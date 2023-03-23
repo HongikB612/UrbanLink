8451008c60b74c94c4a105601456fd3fbedaba81
SoHyun Lee <2887thgus@gmail.com>
2023-03-23 22:44:41 +0900

    Feat Add the contents of postedpage

--- lib/pages/postpage/postedpage.dart
+++ lib/pages/postpage/postedpage.dart
@@ -1,20 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:urbanlink_project/models/user.dart';
+import 'package:urbanlink_project/repositories/post_database_service.dart';
+import 'package:urbanlink_project/repositories/user_database_service.dart';
+import 'package:urbanlink_project/models/posts.dart';
 
 class PostedPage extends StatelessWidget {
   const PostedPage({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Post'),
-      ),
-      body: const Center(
-        child: Text(
-          '게시글입니다.',
-          style: TextStyle(fontSize: 30),
-        ),
-      ),
+    final post = Get.arguments;
+    return StreamBuilder<MyUser?>(
+      stream: UserDatabaseService.getUserStreamById(post.postAuthorId),
+      builder: (context, snapshot) {
+        final authorName = snapshot.data?.userName ?? 'Unknown';
+        return Scaffold(
+          body: Card(
+            child: ListTile(
+              title: Text(
+                post.postTitle,
+              ),
+              subtitle: Column(
+                children: [
+                  Text(
+                    post.postContent,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        authorName,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text(
+                        ' | ',
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '${post.postCreatedTime.month}/${post.postCreatedTime.day}/${post.postCreatedTime.year}',
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
