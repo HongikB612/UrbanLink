055c35f58de41e7885fe2aca6db4f9e00833a01e
nx006 <limazero14@gmail.com>
2023-03-26 17:12:19 +0900

    Feat Replace location data with hard coded location

--- lib/widgets/location_searchbar_widget.dart
+++ lib/widgets/location_searchbar_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:geocoding/geocoding.dart';
-import 'package:logger/logger.dart';
+import 'package:urbanlink_project/services/auth.dart';
 
 class LocationSearchbar extends StatefulWidget {
   final ValueChanged<String> onChanged;
@@ -16,27 +15,45 @@ class _LocationSearchbarState extends State<LocationSearchbar> {
   final List<String> _locationList = [];
   String _selectedLocation = '';
 
+  List<String> fakeLocations = [
+    "서울 마포구 창천동",
+    "서울 마포구 동교동",
+    "서울 서대문구 창천동",
+    "서울 마포구 서강동",
+    "서울 마포구 노고산동",
+    "서울 마포구 서교동",
+    "서울 마포구 상수동",
+    "서울 마포구 신수동",
+    "서울 마포구 구수동",
+    "서울 마포구 하중동",
+    "서울 마포구 신정동",
+    "서울 마포구 대흥동",
+    "서울 마포구 연남동",
+    "서울 마포구 현석동",
+    "서울 마포구 당인동",
+    "서울 마포구 용강동",
+    "서울 서대문구 대현동",
+    "서울 서대문구 신촌동",
+    "서울 마포구 염리동",
+    "서울 서대문구 대신동",
+    "서울 마포구 토정동",
+    "서울 마포구 망원제1동",
+    "서울 마포구 합정동",
+    "서울 마포구 성산제1동"
+  ];
+
   Future<void> _searchLocation(String query) async {
     try {
-      List<Location> locations = await locationFromAddress(query);
-
-      List<String> locationList = [];
-      for (var location in locations) {
-        List<Placemark> placemarks = await placemarkFromCoordinates(
-            location.latitude, location.longitude);
-        for (var placemark in placemarks) {
-          String formattedAddress =
-              '${placemark.street}, ${placemark.subLocality}, ${placemark.locality}, ${placemark.country}';
-          locationList.add(formattedAddress);
-        }
-      }
-
+      List<String> results = [];
+      results.addAll(fakeLocations);
+      results.retainWhere(
+          (location) => location.toLowerCase().contains(query.toLowerCase()));
       setState(() {
         _locationList.clear();
-        _locationList.addAll(locationList);
+        _locationList.addAll(results);
       });
     } catch (e) {
-      Logger().e(e);
+      logger.e(e);
     }
   }
 
