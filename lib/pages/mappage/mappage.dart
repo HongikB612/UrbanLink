e6ce4babd7f559a79a3f993d433713dfe8c3db60
nx006 <limazero14@gmail.com>
2023-03-27 17:50:18 +0900

    Refactor Remove unused import, add const modifier

--- lib/pages/mappage/mappage.dart
+++ lib/pages/mappage/mappage.dart
@@ -1,23 +1,21 @@
-
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_maps/maps.dart';
 import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
-import 'package:urbanlink_project/pages/postpage/postspage.dart';
 import 'dart:async';
-import 'package:get/get.dart';
 import 'package:urbanlink_project/services/auth.dart';
 import 'package:urbanlink_project/widgets/location_drawer_widget.dart';
-import 'package:syncfusion_flutter_maps/maps.dart';
 
 class MapPage extends StatefulWidget {
+  const MapPage({super.key});
+
   @override
-  _AnimateGroupOfMarkersDynamicallyState createState() =>
-      _AnimateGroupOfMarkersDynamicallyState();
+  State<MapPage> createState() => _AnimateGroupOfMarkersDynamicallyState();
 }
 
-class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage> with TickerProviderStateMixin {
+class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage>
+    with TickerProviderStateMixin {
   late AnimationController _controller;
 
   late CurvedAnimation _animation;
@@ -38,15 +36,15 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage> with TickerP
     _tileLayerController = MapTileLayerController();
 
     _markers = <String, MapLatLng>{
-      'Chad': MapLatLng(15.454166, 18.732206),
-      'Nigeria': MapLatLng(9.081999, 8.675277),
-      'DRC': MapLatLng(-4.038333, 21.758663),
-      'CAR': MapLatLng(6.600281, 20.480205),
-      'Sudan': MapLatLng(12.862807, 30.217636),
-      'Kenya': MapLatLng(0.0236, 37.9062),
-      'Zambia': MapLatLng(-10.974129, 30.861397),
-      'Egypt': MapLatLng(25.174109, 28.776359),
-      'Algeria': MapLatLng(24.276672, 7.308186),
+      'Chad': const MapLatLng(15.454166, 18.732206),
+      'Nigeria': const MapLatLng(9.081999, 8.675277),
+      'DRC': const MapLatLng(-4.038333, 21.758663),
+      'CAR': const MapLatLng(6.600281, 20.480205),
+      'Sudan': const MapLatLng(12.862807, 30.217636),
+      'Kenya': const MapLatLng(0.0236, 37.9062),
+      'Zambia': const MapLatLng(-10.974129, 30.861397),
+      'Egypt': const MapLatLng(25.174109, 28.776359),
+      'Algeria': const MapLatLng(24.276672, 7.308186),
     };
 
     _controller.repeat(min: 0.1, max: 1.0, reverse: true);
@@ -66,21 +64,29 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage> with TickerP
     bool isbuttonPressed = false;
     bool selected = false;
     return Scaffold(
-      appBar: AppBar(title: Text('Animate group of markers dynamically')),
+      appBar: AppBar(title: const Text('Animate group of markers dynamically')),
       endDrawer: const LocationDrawerWidget(),
       body: Stack(
         children: [
           MapTileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             initialZoomLevel: 3,
-            initialFocalLatLng: MapLatLng(2.3104, 16.5581),
+            initialFocalLatLng: const MapLatLng(2.3104, 16.5581),
             controller: _tileLayerController,
             initialMarkersCount: _markers.length,
-            zoomPanBehavior: MapZoomPanBehavior(enableMouseWheelZooming: true, showToolbar: true,),
+            zoomPanBehavior: MapZoomPanBehavior(
+              enableMouseWheelZooming: true,
+              showToolbar: true,
+            ),
             markerBuilder: (BuildContext context, int index) {
-              final double size = _selectedMarkerIndices.contains(index) ? 40 : 25;
+              final double size =
+                  _selectedMarkerIndices.contains(index) ? 40 : 25;
               final MapLatLng markerLatLng = _markers.values.elementAt(index);
-              Widget current = Icon(Icons.circle, color: isbuttonPressed ? Colors.pinkAccent.withOpacity(0.5) : Colors.lightBlueAccent.withOpacity(0.5), size: size);
+              Widget current = Icon(Icons.circle,
+                  color: isbuttonPressed
+                      ? Colors.pinkAccent.withOpacity(0.5)
+                      : Colors.lightBlueAccent.withOpacity(0.5),
+                  size: size);
               return MapMarker(
                 latitude: markerLatLng.latitude,
                 longitude: markerLatLng.longitude,
@@ -88,24 +94,25 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage> with TickerP
                   onTap: () {
                     selected = true;
                   },
-                  child: selected ? Transform.translate(
-                    offset: Offset(0.0, -size),
-                    child: _selectedMarkerIndices.contains(index)
-                        ? ScaleTransition(
-                        alignment: Alignment.bottomCenter,
-                        scale: _animation,
-                        child: current)
-                        : current,
-                  ) :
-                  Transform.translate(
-                    offset: Offset(0.0, -size / 2),
-                    child: _selectedMarkerIndices.contains(index)
-                        ? ScaleTransition(
-                        alignment: Alignment.bottomCenter,
-                        scale: _animation,
-                        child: current)
-                        : current,
-                  ),
+                  child: selected
+                      ? Transform.translate(
+                          offset: Offset(0.0, -size),
+                          child: _selectedMarkerIndices.contains(index)
+                              ? ScaleTransition(
+                                  alignment: Alignment.bottomCenter,
+                                  scale: _animation,
+                                  child: current)
+                              : current,
+                        )
+                      : Transform.translate(
+                          offset: Offset(0.0, -size / 2),
+                          child: _selectedMarkerIndices.contains(index)
+                              ? ScaleTransition(
+                                  alignment: Alignment.bottomCenter,
+                                  scale: _animation,
+                                  child: current)
+                              : current,
+                        ),
                 ),
               );
             },
@@ -131,7 +138,7 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage> with TickerP
       ),
       floatingActionButton: GestureDetector(
         child: FloatingActionButton(
-          child: Icon(Icons.animation),
+          child: const Icon(Icons.animation),
           onPressed: () {
             setState(() {
               isbuttonPressed = !isbuttonPressed;
@@ -142,24 +149,25 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage> with TickerP
           },
         ),
       ),
-
     );
   }
+
   Future<void> _searchLocation(String query) async {
     try {
       List<Location> locations = await locationFromAddress(query);
       if (locations.isNotEmpty) {
         final LatLng newCenter =
-        LatLng(locations.first.latitude, locations.first.longitude);
+            LatLng(locations.first.latitude, locations.first.longitude);
         final focusLatLng = MapLatLng(newCenter.latitude, newCenter.longitude);
 
         setState(() {
-          _tileLayerController.pixelToLatLng(Offset(focusLatLng.latitude, focusLatLng.longitude));
+          _tileLayerController.pixelToLatLng(
+              Offset(focusLatLng.latitude, focusLatLng.longitude));
         });
-      }
-      else {
+      } else {
         setState(() {
-          _tileLayerController.pixelToLatLng(Offset(15.454166, 18.732206));
+          _tileLayerController
+              .pixelToLatLng(const Offset(15.454166, 18.732206));
         });
       }
     } catch (e) {
