eddc2a98f9df0389f4621a10daf9cf60da7a1cb5
nx006 <limazero14@gmail.com>
2023-03-27 18:01:53 +0900

    Fix Key index descriptive

--- lib/pages/mappage/mappage.dart
+++ lib/pages/mappage/mappage.dart
@@ -5,7 +5,6 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:anim_search_bar/anim_search_bar.dart';
 import 'dart:async';
 import 'package:urbanlink_project/services/auth.dart';
-import 'package:urbanlink_project/widgets/location_drawer_widget.dart';
 
 class MapPage extends StatefulWidget {
   const MapPage({super.key});
@@ -36,23 +35,22 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage>
     _tileLayerController = MapTileLayerController();
 
     _markers = <String, MapLatLng>{
-      'Sogu' : MapLatLng(37.552635722509, 126.92436042413),
-      'Mapo' : MapLatLng(37.565643683342, 126.95524147826),
-      'SangSu' : MapLatLng(37.495172947072, 126.95453489844),
-      'GanAk' : MapLatLng(37.47538611, 126.9538444),
-      'GanAk' : MapLatLng(37.53573889, 127.0845333),
-      'GanAk' : MapLatLng(37.49265, 126.8895972),
-      'GanAk' : MapLatLng(37.44910833, 126.9041972),
-      'Chad': MapLatLng(15.454166, 18.732206),
-      'Nigeria': MapLatLng(9.081999, 8.675277),
-      'DRC': MapLatLng(-4.038333, 21.758663),
-      'CAR': MapLatLng(6.600281, 20.480205),
-      'Sudan': MapLatLng(12.862807, 30.217636),
-      'Kenya': MapLatLng(0.0236, 37.9062),
-      'Zambia': MapLatLng(-10.974129, 30.861397),
-      'Egypt': MapLatLng(25.174109, 28.776359),
-      'Algeria': MapLatLng(24.276672, 7.308186),
-
+      'Sogu': const MapLatLng(37.552635722509, 126.92436042413),
+      'Mapo': const MapLatLng(37.565643683342, 126.95524147826),
+      'SangSu': const MapLatLng(37.495172947072, 126.95453489844),
+      'GanAk0': const MapLatLng(37.47538611, 126.9538444),
+      'GanAk1': const MapLatLng(37.53573889, 127.0845333),
+      'GanAk2': const MapLatLng(37.49265, 126.8895972),
+      'GanAk3': const MapLatLng(37.44910833, 126.9041972),
+      'Chad': const MapLatLng(15.454166, 18.732206),
+      'Nigeria': const MapLatLng(9.081999, 8.675277),
+      'DRC': const MapLatLng(-4.038333, 21.758663),
+      'CAR': const MapLatLng(6.600281, 20.480205),
+      'Sudan': const MapLatLng(12.862807, 30.217636),
+      'Kenya': const MapLatLng(0.0236, 37.9062),
+      'Zambia': const MapLatLng(-10.974129, 30.861397),
+      'Egypt': const MapLatLng(25.174109, 28.776359),
+      'Algeria': const MapLatLng(24.276672, 7.308186),
     };
 
     _controller.repeat(min: 0.1, max: 1.0, reverse: true);
@@ -78,7 +76,8 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage>
           MapTileLayer(
             urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
             initialZoomLevel: 3,
-            initialFocalLatLng: const MapLatLng(37.565643683342, 126.95524147826),
+            initialFocalLatLng:
+                const MapLatLng(37.565643683342, 126.95524147826),
             controller: _tileLayerController,
             initialMarkersCount: _markers.length,
             zoomPanBehavior: MapZoomPanBehavior(
@@ -86,7 +85,8 @@ class _AnimateGroupOfMarkersDynamicallyState extends State<MapPage>
               showToolbar: true,
             ),
             markerBuilder: (BuildContext context, int index) {
-              final double size = _selectedMarkerIndices.contains(index) ? 400 : 300;
+              final double size =
+                  _selectedMarkerIndices.contains(index) ? 400 : 300;
               final MapLatLng markerLatLng = _markers.values.elementAt(index);
               Widget current = Icon(Icons.circle,
                   color: isbuttonPressed
